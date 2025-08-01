585c2de9475402128f7f6710d10e29692fbcbe4f
Saad Mirza <saadmirza729@gmail.com>
2025-08-01 11:54:23 +0500

    updated new files

---
+++ Assignment 1 js/app.js
@@ -0,0 +1,53 @@
+alert("Password is incorrect!");
+
+alert("Write it carefully");
+
+var firstname = "Mr.Saad";
+firstname = "Saad";
+
+var lastname = "Call Me Mirza";
+lastname = "Mirza";
+
+var mynationality = "U.S";
+mynationality = "Pakistan";
+
+var thanks = "Thanks you for Entering"
+alert(thanks);
+
+console.log("my name is" + " " + firstname + " " + lastname);
+
+var num1 = "50";
+var num2 = "50";
+
+console.log(num1 + num2);
+
+
+
+var weight = 200;
+
+weight = 200;
+
+var result1 = weight + 100;
+
+var numone = 93;
+var numtwo = 7;
+
+var mainnum = numone + numtwo;
+
+var value = 40;
+value = value + 10;
+
+
+// We can we like this, use number after word in variable not before, so it will not work
+
+var subject1 = "Monday"
+
+
+
+
+
+
+
+
+
+
